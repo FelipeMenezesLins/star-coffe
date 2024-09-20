bd8b7151879f0f12a941b11519814778e149e476
Felipe Menezes Lins <felipemenezesmk22@gmail.com>
2024-09-20 14:07:10 -0300

    att

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 let categorias = document.querySelectorAll('.chose')
 let ul = document.getElementById('opcao')
-
+console.log(ul)
 
 window.addEventListener('scroll', function() {
     const nav = document.getElementById('nav-1');
@@ -11,18 +11,21 @@ window.addEventListener('scroll', function() {
       nav.classList.add('show');
     } else {
       nav.classList.remove('show');
-      ul.style.display = 'none'
+      ul.classList.remove('apresentar')
     }
   });
 
-  categorias.forEach((categorias)=> {
-    categorias.addEventListener('click', ()=> {
-        if(ul.style.display == 'none') {
-            ul.style.display = 'block'
-        } else {
-            ul.style.display = 'none'
+  categorias.forEach((categoria) => {
+    categoria.addEventListener('click', () => {
+        // Certifique-se de que o ul existe
+        if (ul) {
+            ul.style.opacity = '1'
+            ul.classList.add('apresentar');
         }
-    })
+    });
+});
+
+ul.addEventListener('mouseleave', ()=> {
+  ul.classList.remove('apresentar')
+  ul.style.opacity = '0'
 })
-
-
